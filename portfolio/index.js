cd8ff879a5553a4aaedfea1cce9ea1c340aeb265
Vadzim PC <comtvset@gmail.com>
2022-01-31 17:52:49 +0100

    fix: cash and add active

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,14 +1,14 @@
-const hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.hamburger')
 
 function toggleMenu() {
-  hamburger.classList.toggle('open');
+  hamburger.classList.toggle('open')
 }
-hamburger.addEventListener('click', toggleMenu);
+hamburger.addEventListener('click', toggleMenu)
 
 
-const portfolioBtn = document.querySelector('.portfolio-button');
-const portfolioImages = document.querySelectorAll('.portfolio-image');
-const portfolioBtns = document.querySelector('.wrapp_portfolio1');
+const portfolioBtn = document.querySelector('.portfolio-button', '.intro-button', '.contact-button', 'pricebutton_style')
+const portfolioImages = document.querySelectorAll('.portfolio-image')
+const portfolioBtns = document.querySelector('.wrapp_portfolio1')
 
 // portfolioBtn.addEventListener('click', () => {
 //   portfolioImages.forEach((img, index) => img.src = `./assets/img/winter/${index + 1}.jpg`)
@@ -18,29 +18,33 @@ const portfolioBtns = document.querySelector('.wrapp_portfolio1');
 function changeImage(event) {
   if (event.target.classList.contains('portfolio-button')) {
      if(event.target.dataset.season === 'winter') {
-        portfolioImages.forEach((img, index) => img.src = `./assets/img/winter/${index + 1}.jpg`);
+        portfolioImages.forEach((img, index) => img.src = `./assets/img/winter/${index + 1}.jpg`)
 
      } else if (event.target.dataset.season === 'spring') {
-        portfolioImages.forEach((img, index) => img.src = `./assets/img/spring/${index + 1}.jpg`);
+        portfolioImages.forEach((img, index) => img.src = `./assets/img/spring/${index + 1}.jpg`)
 
      } else if (event.target.dataset.season === 'summer') {
-        portfolioImages.forEach((img, index) => img.src = `./assets/img/summer/${index + 1}.jpg`);
+        portfolioImages.forEach((img, index) => img.src = `./assets/img/summer/${index + 1}.jpg`)
 
      } else if (event.target.dataset.season === 'autumn') {
-        portfolioImages.forEach((img, index) => img.src = `./assets/img/autumn/${index +1}.jpg`);
+        portfolioImages.forEach((img, index) => img.src = `./assets/img/autumn/${index +1}.jpg`)
      }
-  };
-};
+  }
+}
 portfolioBtns.addEventListener('click', changeImage);
 
 //Cash image
-const seasons = document.querySelectorAll(['winter', 'spring', 'summer', 'autumn']);
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
 function preloadImages(array) {
-array.forEach((seasons) => {
+  array.forEach((seasons) => {
   for(let i = 1; i <= 6; i++) {
-    const img = new Image();
-    img.src = `./assets/img/${seasons}/${i}.jpg`;
+    const img = new Image()
+    img.src = `./assets/img/${seasons}/${i}.jpg`
   }
 })
 }
-preloadImages();
+preloadImages(seasons)
+
+//Active button
+
+portfolioBtn.classList.add('active');
