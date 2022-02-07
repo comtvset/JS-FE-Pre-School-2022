d7c03ffad37af1849829a1deff0769dc384c0082
Vadzim PC <comtvset@gmail.com>
2022-02-07 05:08:32 +0100

    add: js function

--- js30_1.2-audio-player/index.js
+++ js30_1.2-audio-player/index.js
@@ -7,7 +7,8 @@ const player = document.querySelector('.player'),
       progress = document.querySelector('.progress'),
       titel = document.querySelector('.title_song'),
       cover = document.querySelector('.cover_img'),
-      imgSrc = document.querySelector('.img_src')
+      imgSrc = document.querySelector('.img_src'),
+      time = document.querySelector('time')
 
 const songs = ['The Architect - Cretin De Terrien', 'Sudan Archives - Come Meh Way', 'Love Beans - Who Is She']
 
@@ -16,18 +17,73 @@ let songIndex = 0
 function initSong(song) {
  titel.innerHTML = song
  audio.src = `./assets/audio/${song}.mp3`
-//  cover.src = `./assets/img/${songIndex}.jpg`
+ cover.src = `./assets/svg/cover4.svg`
 }
 initSong(songs[songIndex])
 
 function playSong() {
+  player.classList.add('play')
+  cover.classList.add('active')
+  imgSrc.src = './assets/svg/pause.svg'
   audio.play()
 }
 
 function pauseSong() {
+  player.classList.remove('play')
+  cover.classList.remove('active')
+  imgSrc.src = './assets/svg/play.svg'
   audio.pause()
 }
 
 playBtn.addEventListener('click', () => {
-  playSong()
+  const isPlaying = player.classList.contains('play')
+  if (isPlaying) {
+    pauseSong()
+  } else {
+    playSong()
+  }
 })
+
+function nextSong() {
+  songIndex++
+  if (songIndex > songs.length -1) {
+    songIndex = 0
+  }
+  initSong(songs[songIndex])
+  playSong()
+}
+forwardBtn.addEventListener('click', nextSong)
+
+function backSong() {
+  songIndex--
+  if (songIndex < 0) {
+    songIndex = songs.length -1
+  }
+  initSong(songs[songIndex])
+  playSong()
+}
+backBtn.addEventListener('click', backSong)
+
+function updateProgress(event) {
+  const {duration, currentTime} = event.srcElement
+  const progressPercent = (currentTime / duration) * 100
+  progress.style.width = `${progressPercent}%`
+}
+audio.addEventListener('timeupdate', updateProgress)
+
+function setProgress(event) {
+  const width = this.clientWidth
+  const clickX = event.offsetX
+  const duration = audio.duration
+
+  audio.currentTime = (clickX / width) * duration
+}
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
+
+function setTime() {
+  audio.onloadeddata = audio.duration
+  
+}
+audio.addEventListener('timeupdate', setTime)
