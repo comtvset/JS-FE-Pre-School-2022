0f7ac4235aaa837f7b645b2356a1a4736cb81045
Vadzim PC <comtvset@gmail.com>
2022-02-21 02:21:09 +0100

    fix: html, css, js

--- js30_3.1-tic-tac-toe/index.js
+++ js30_3.1-tic-tac-toe/index.js
@@ -1,1 +1,38 @@
-.
+const field = document.querySelector('.field')
+let move = 0;
+
+field.addEventListener('click', function(event) {
+    if(move % 2 === 0) {
+        event.target.innerHTML = 'X'
+    } else {event.target.innerHTML = 'O'}
+    move++
+    check()
+})
+
+function check() {
+    const boxes = document.getElementsByClassName('box')
+    const win = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+
+        [0,4,8],
+        [6,4,2]
+    ]
+
+    for(let i = 0; i < win.length; i++) {
+        if(boxes[win[i][0]].innerHTML === 'X' && boxes[win[i][1]].innerHTML === 'X' && boxes[win[i][2]].innerHTML === 'X') {
+            alert('X - WIN!')
+            location.reload()
+        } else if(boxes[win[i][0]].innerHTML === 'O' && boxes[win[i][1]].innerHTML === 'O' && boxes[win[i][2]].innerHTML === 'O'){
+            alert('O - WIN!')
+            location.reload()
+        } else {
+            
+        }
+    }
+}
